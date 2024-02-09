3e3bb5e66696d958f3c6a83e2d39addb475cd593
LukaVDVC <120012190+LukaVDVC@users.noreply.github.com>
2024-02-09 17:36:54 +0100

    Update strings4.rs

--- exercices/09_strings/strings4.rs
+++ exercices/09_strings/strings4.rs
@@ -10,21 +10,19 @@
 // I AM NOT DONE
 
 fn string_slice(arg: &str) {
-    println!("{}", arg);
+println!("{}", arg);
 }
-fn string(arg: String) {
-    println!("{}", arg);
+fn string(arg: String) {a
+println!("{}", arg);
 }
-
 fn main() {
-    ???("blue");
-    ???("red".to_string());
-    ???(String::from("hi"));
-    ???("rust is fun!".to_owned());
-    ???("nice weather".into());
-    ???(format!("Interpolation {}", "Station"));
-    ???(&String::from("abc")[0..1]);
-    ???("  hello there ".trim());
-    ???("Happy Monday!".to_string().replace("Mon", "Tues"));
-    ???("mY sHiFt KeY iS sTiCkY".to_lowercase());
+string_slice("blue");
+string("red".to_string());string(String::from("hi"));
+string("rust is fun!".to_owned());
+string("nice weather".to_owned());
+string(format!("Interpolation {}", "Station"));
+string_slice(&String::from("abc")[0..1].to_string());
+string(" hello there ".trim().to_string());
+string("Happy Monday!".to_string().replace("Mon", "Tues"));
+string("mY sHiFt KeY iS sTiCkY".to_lowercase());
 }
