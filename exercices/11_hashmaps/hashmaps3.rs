794302b5fb20556303a099a58f92b0c8d00375d4
LukaVDVC <120012190+LukaVDVC@users.noreply.github.com>
2024-02-09 17:39:00 +0100

    Update hashmaps3.rs

--- exercices/11_hashmaps/hashmaps3.rs
+++ exercices/11_hashmaps/hashmaps3.rs
@@ -38,4 +38,39 @@ team2.goals_scored += team_2_score;
 team2.goals_conceded += team_1_score;
 }
 scores
+}#[cfg(test)]
+mod tests {
+use super::*;
+fn get_results() -> String {
+let results = "".to_string()
++ "England,France,4,2\n"
++ "France,Italy,3,1\n"
++ "Poland,Spain,2,0\n"
++ "Germany,England,2,1\n";
+results
+}
+#[test]
+fn build_scores() {
+let scores = build_scores_table(&get_results());
+let mut keys: Vec<&String> = scores.keys().collect();
+keys.sort();
+assert_eq!(
+keys,
+vec!["England", "France", "Germany", "Italy", "Poland", "Spain"]
+);
+}
+#[test]
+fn validate_team_score_1() {
+let scores = build_scores_table(&get_results());
+let team = scores.get("England").unwrap();
+assert_eq!(team.goals_scored, 5);
+assert_eq!(team.goals_conceded, 4);
+}
+#[test]
+fn validate_team_score_2() {
+let scores = build_scores_table(&get_results());
+let team = scores.get("Spain").unwrap();
+assert_eq!(team.goals_scored, 0);
+assert_eq!(team.goals_conceded, 2);
+}
 }
